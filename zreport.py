2643ff93f6c06fdd1c320bcf54daebae3e990e89
bukhal <bukhal@i-teco.ru>
2015-09-03 11:19:12 +0300

    - ftp destination login added - archiving logic added - logger refactoring - report xml validation logic added

--- zreport.py
+++ zreport.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ######################################################################################################
-# created by Bukhal Peter <bukhal at i-teco.ru>
+# created by Peter Bukhal <bukhal at i-teco.ru>
 ######################################################################################################
 
 import os
@@ -10,45 +10,57 @@ import logging
 import logging.handlers
 import glob
 import sys
+import xml.etree.ElementTree as et
+import ftplib
+import shutil
+import zipfile
 
 # Paths configuration
-ZREPORT_DIRECTORY="data"
-ZREPORT_DIRECTORY_CHECK_INTERVAL=3
-ZREPORT_ARCHIVE_DIRECTORY="archive"
+REPORT_DIRECTORY = "data"
+REPORT_DIRECTORY_CHECK_INTERVAL = 3
+ARCHIVE_DIRECTORY = "archive"
 
 # Task configuration
-TASK_PID=str(os.getpid())
-TASK_LOCK_FILE="zreport.lock"
-TASK_TOTAL_EXECUTION_TIME=""
-TASK_MAX_EXECUTION_TIME=10 # In seconds
-TASK_ALLOW_MULTIPLE_INSTANSES=False
+TASK_PID = str(os.getpid())
+TASK_LOCK_FILE = "zreport.lock"
+TASK_TOTAL_EXECUTION_TIME = ""
+TASK_MAX_EXECUTION_TIME = 10  # In seconds
+TASK_ALLOW_MULTIPLE_INSTANCES = False
 
 # Logging configuration
-LOG_DIR="log/"
-LOG_FILE=LOG_DIR+"zreport.log"
-LOG_FILE_MAX_SIZE=1024 * 1024 * 2 # 2Mb
-LOG_FILE_BACKUP_COUNT=5
+LOG_DIR = "log"
+LOG_FILE = LOG_DIR+"/zreport.log"
+LOG_FILE_MAX_SIZE = 1024 * 1024 * 2  # 2Mb
+LOG_FILE_BACKUP_COUNT = 5
 
 # FTP connection configuration
-FTP_USERNAME="zreport"
-FTP_PASSWORD="QAwKySdTwk"
+FTP_SERVER = "localhost"
+FTP_SERVER_PATH = "./IdeaProjects/zreport/ftpdest1"
+FTP_USERNAME = "peterbukhal"
+FTP_PASSWORD = "123"
 
-# Create log folder if it not exists
-if not os.path.isdir(LOG_DIR):
-    os.mkdir(LOG_DIR)
+# Initialize logger
+def init_logger():
+    # Create log folder if it not exists
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
 
-logger = logging.getLogger("zReport")
-logger.setLevel("INFO")
-defaultFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s {pid=%(process)d}: %(message)s")
+    logger = logging.getLogger("zReport")
+    logger.setLevel("INFO")
+    default_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s {pid=%(process)d}: %(message)s")
 
-fileHandler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=LOG_FILE_MAX_SIZE, backupCount=LOG_FILE_BACKUP_COUNT)
-fileHandler.setFormatter(defaultFormatter)
+    file_handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=LOG_FILE_MAX_SIZE, backupCount=LOG_FILE_BACKUP_COUNT)
+    file_handler.setFormatter(default_formatter)
 
-consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(defaultFormatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(default_formatter)
 
-logger.addHandler(fileHandler)
-logger.addHandler(consoleHandler)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+main_logger = init_logger()
 
 # Checks if execution time exceeded
 def time_exceeded():
@@ -59,26 +71,72 @@ def time_exceeded():
     else:
         return False
 
-#
+class ZReportDestination:
+
+    def __init__(self, ftp_server, ftp_username, ftp_password, ftp_server_path):
+        self.ftp_server = ftp_server
+        self.ftp_username = ftp_username
+        self.ftp_password = ftp_password
+        self.ftp_server_path = ftp_server_path
+
+        self.ftp = ftplib.FTP(self.ftp_server)
+        self.ftp.login(self.ftp_username, self.ftp_password)
+        self.ftp.cwd(self.ftp_server_path)
+
+    def send_report(self, report):
+        # STOR report file to server
+        with open(report, "r") as report_file:
+            self.ftp.storlines("STOR " + os.path.basename(report), report_file)
+            main_logger.info("Report " + os.path.basename(report) + " was successfully sent to destination server")
+            archive_report(report)
+
+# Validates reports to be a valid xml document
+# and send it to a server destination
 def send_report_to_server(report):
-    logger.info("Sending report " + report)
+    main_logger.info("Sending report " + os.path.basename(report) + "...")
+
+    # validate xml document
+    try:
+        tree = et.parse(report)
+        root = tree.getroot()
+    except et.ParseError:
+        main_logger.warn("Report " + os.path.basename(report) + " is not well-formed xml document, skip it")
+    else:
+        # Send report to server
+        ftp_destination = ZReportDestination(FTP_SERVER, FTP_USERNAME, FTP_PASSWORD, FTP_SERVER_PATH)
+        ftp_destination.send_report(report)
+
+# Move sent reports to archive directory,
+# remove from archive directory old reports
+def archive_report(report):
+    if not os.path.isdir(ARCHIVE_DIRECTORY):
+        os.mkdir(ARCHIVE_DIRECTORY)
+
+    archived_report = ARCHIVE_DIRECTORY + "/" + os.path.basename(report)
+
+    with zipfile.ZipFile(ARCHIVE_DIRECTORY + "/archive.zip", "w") as archive:
+        shutil.copy(report, archived_report)
+        archive.write(archived_report)
+        main_logger.info("Report " + os.path.basename(report) + " was successfully archived")
 
 #
 def wait_for_reports():
-    logger.info("Waiting for reports... (check it every ~" + str(ZREPORT_DIRECTORY_CHECK_INTERVAL) + " seconds)")
+    main_logger.info("Waiting for reports... (check it every ~" + str(REPORT_DIRECTORY_CHECK_INTERVAL) + " seconds)")
     while True:
-        for report in glob.glob(ZREPORT_DIRECTORY + "/ZReport*.xml"):
-            send_report_to_server(os.path.basename(report))
+        for report in glob.glob(REPORT_DIRECTORY + "/ZReport*.xml"):
+            send_report_to_server(report)
 
         if time_exceeded():
-            logger.info("Task has exceeded max execution time and will be stopped normally")
+            main_logger.info("Task has exceeded max execution time and will be stopped normally")
 
             return
         else:
-            time.sleep(ZREPORT_DIRECTORY_CHECK_INTERVAL)
+            time.sleep(REPORT_DIRECTORY_CHECK_INTERVAL)
 
-#
-def check_pid(pid):
+# Tries to terminate given process by it`s PID
+# if fails returns False
+# else if process with PID was terminated returns True
+def terminate_pid(pid):
     try:
         os.kill(int(pid), 9)
     except OSError:
@@ -89,62 +147,62 @@ def check_pid(pid):
 #
 def check_lock_file():
     if os.path.isfile(TASK_LOCK_FILE):
-        with open(TASK_LOCK_FILE,"r") as lock_file:
+        with open(TASK_LOCK_FILE, "r") as lock_file:
             lock_pid = lock_file.readline()
 
-        logger.warn("Task w/ pid " + lock_pid + " already running")
+        main_logger.warn("Task w/ pid " + lock_pid + " already running")
         while True:
             if os.path.isfile(TASK_LOCK_FILE):
                 diff = int(time.time() - os.path.getctime(TASK_LOCK_FILE))
 
                 if diff > TASK_MAX_EXECUTION_TIME:
-                    logger.warn("Task w/ pid " + lock_pid + " has exceeded max execution time and will be terminated")
+                    main_logger.warn("Task w/ pid " + lock_pid + " has exceeded max execution time and will be terminated")
 
-                    if check_pid(lock_pid):
-                        logger.warn("Task w/ pid " + lock_pid + " was successfully terminated")
+                    if terminate_pid(lock_pid):
+                        main_logger.warn("Task w/ pid " + lock_pid + " was successfully terminated")
                     else:
-                        logger.warn("Task process w/ pid " + lock_pid + " was not found, skip task termination")
+                        main_logger.warn("Task process w/ pid " + lock_pid + " was not found, skip task termination")
 
                     remove_lock_file()
 
                     return True
                 else:
-                    # If another tast is running and TASK_ALLOW_MULTIPLE_INSTANSES enabled
+                    # If another task is running and TASK_ALLOW_MULTIPLE_INSTANCES enabled
                     # wait for another task to complete else stop itself
-                    if TASK_ALLOW_MULTIPLE_INSTANSES:
-                        logger.warn("wait for 5 seconds...")
+                    if TASK_ALLOW_MULTIPLE_INSTANCES:
+                        main_logger.warn("wait for 5 seconds...")
                         time.sleep(5)
                     else:
-                        logger.warn("Multiple tasks execution is disabled, exiting...")
+                        main_logger.warn("Multiple tasks execution is disabled, exiting...")
                         return False
             else:
-                logger.warn("Task w/ pid " + lock_pid + " has completed, resume...")
+                main_logger.warn("Task w/ pid " + lock_pid + " has completed, resume...")
                 return True
     else:
         return True
 
-#
+# Creates lock file and write process PID in to it
 def create_lock_file():
-    with open(TASK_LOCK_FILE,"w") as lock_file:
+    with open(TASK_LOCK_FILE, "w") as lock_file:
         lock_file.write(TASK_PID)
 
-#
+# Removes lock file if it exists else does nothing
 def remove_lock_file():
     if os.path.isfile(TASK_LOCK_FILE):
         os.remove(TASK_LOCK_FILE)
 
-#
+# Initializes zReport task
 def init():
     if check_lock_file():
         create_lock_file()
-        logger.info("Started")
+        main_logger.info("Started")
     else:
         sys.exit(0)
 
-#
+# Finish zReport task
 def finish():
     remove_lock_file()
-    logger.info("Completed")
+    main_logger.info("Completed")
 
 init()
 
